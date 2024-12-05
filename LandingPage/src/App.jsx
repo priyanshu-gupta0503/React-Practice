b2bbc2fb4db6cce9da77568a6e48686819f4a9eb
Priyanshu Gupta <priyanshu1535@gmail.com>
2024-12-05 16:14:09 +0530

    Made a landing page

---
+++ LandingPage/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import "./App.css"
+import Main from "./components/Main/Main"
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Main/>
+    </>
+  
+  )
+}
+
+export default App
---
+++ LandingPage/src/components/Main/Main.jsx
@@ -0,0 +1,30 @@
+// import React from 'react'
+
+function Main() {
+  return (
+    <>
+    <main className="main">
+        <div className="main=content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="main-button">
+                <button>Shop Now</button>
+                <button  className="bt1">Category</button>
+            </div>
+            <div className="shopping">
+                Also Available
+                <div className="but"> 
+                    <img src="/images/amazon.png" alt="amazon" />
+                    <img src="/images/flipkart.png" alt="Flipkart" />
+                </div>
+            </div>
+        </div>
+        <div className="main-image">
+            <img src="/images/hero-image.png" alt="" />
+        </div>
+    </main>
+    </>
+  )
+}
+
+export default Main
